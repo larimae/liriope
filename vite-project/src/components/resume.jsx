8e8b76ba8583ce0415977ea837c57c3638029d64
larimae <larissamae60@gmail.com>
2024-11-09 11:11:28 -0800

    added content to resume file with link to download pdf of resume

--- vite-project/src/components/resume.jsx
+++ vite-project/src/components/resume.jsx
@@ -3,7 +3,26 @@ import '../index.css';
 
 const Resume = () => {
     return (
-        <div className="resume-container"></div>
-)};
+        <div className="resume">
+          <h1>Resume</h1>
+          <p>
+            Download my resume:
+            <a href="/Resume.pdf" download className="resume-download">
+              <button>Download Resume</button>
+            </a>
+          </p>
+    
+          <h2>Proficiencies</h2>
+          <ul>
+            <li>JavaScript (ES6+)</li>
+            <li>React, Redux</li>
+            <li>Node.js, Express</li>
+            <li>SQL, PostgreSQL</li>
+            <li>HTML5, CSS3,</li>
+            <li>Git, GitHub</li>
+            <li>Agile Development</li>
+          </ul>
+        </div>
+      )};
 
 export default Resume;
